51b135ccf7bc7a14f1e8f9e1e339284a2b9eb7b9
Ahmed Walid <123060771+ahmed-hq@users.noreply.github.com>
2023-06-24 23:00:42 +0300

    Fetching imgs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-import './styles/main.scss'
+import './styles/main.scss';
+
+async function hamada() {
+  const body = document.body
+  const data = await fetch(
+    'https://api.unsplash.com/photos/', {
+      method: 'GET',
+      headers: {
+        Authorization: `Client-ID xL89z7hn8F3AhY7j8f23z2mNIR9qGyU3338Yw4sdjI4`
+      }
+    }
+  );
+  const res = await data.json();
+  console.log(res);
+  res.map((e) => {
+    const url = e.urls.regular;
+    const img = document.createElement('img')
+    img.setAttribute('src', `${url}`)
+    body.appendChild(img)
+  });
+}
+
+hamada();
