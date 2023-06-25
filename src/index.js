e1808d027c33bf15dc65359ca1d72d9466a70f12
Ahmed Walid <123060771+ahmed-hq@users.noreply.github.com>
2023-06-26 01:03:46 +0300

    Adding api module

--- src/index.js
+++ src/index.js
@@ -1,33 +1,25 @@
 import './styles/main.scss';
+import { api } from './modules/api';
 
-async function hamada() {
-  const body = document.body
-  const data = await fetch(
-    'https://api.unsplash.com/photos/random?count=30&collections=4369173', {
-      method: 'GET',
-      headers: {
-        Authorization: `Client-ID ${process.env.ACCESS_KEY}`
-      }
-    }
-  );
-  const res = await data.json();
-  console.log(res);
-  res.map((e) => {
-    const url = e.urls.regular;
-    const img = document.createElement('img')
-    img.setAttribute('src', `${url}`)
-    body.appendChild(img)
-  });
+async function getData() {
+  const body = document.body;
   
+  await api.getRandomPhotos();
+  api.randomPhotosAPI.map((e) => {
+    const url = e.urls.regular;
+    const img = document.createElement('img');
+    img.setAttribute('src', `${url}`);
+    body.appendChild(img);
+  });
+
   const moreBtn = document.createElement('button');
-  moreBtn.style.display = 'block'
-  moreBtn.innerText = 'More'
-  body.appendChild(moreBtn)
+  moreBtn.style.display = 'block';
+  moreBtn.innerText = 'More';
+  body.appendChild(moreBtn);
   moreBtn.addEventListener('click', () => {
-    hamada()
-    moreBtn.style.display = 'none'
-  })
+    getData();
+    moreBtn.style.display = 'none';
+  });
 }
 
-
-hamada();
+getData();
---
+++ src/modules/api.js
@@ -0,0 +1,21 @@
+class API {
+  constructor(){
+    this.randomPhotosAPI = null;
+  }
+
+  async  getRandomPhotos() {
+    const body = document.body
+    const res = await fetch(
+      'https://api.unsplash.com/photos/random?count=30&collections=4369173', {
+        method: 'GET',
+        headers: {
+          Authorization: `Client-ID ${process.env.ACCESS_KEY}`
+        }
+      }
+    );
+    this.randomPhotosAPI = await res.json();
+  }
+}
+
+const api = new API();
+export { api }
