c880acd871a5426fb81cae847e25cdf105b3acf9
Ahmed Walid <123060771+ahmed-hq@users.noreply.github.com>
2023-06-26 01:51:18 +0300

    Creating Load Btn

--- src/index.js
+++ src/index.js
@@ -2,20 +2,6 @@ import './styles/main.scss';
 import { api } from './modules/api';
 import { ui } from './modules/ui';
 
-async function getData() {
-  const body = document.body;
-  
-  await api.getRandomPhotos();
-  ui.appendPhotos(api.randomPhotosAPI)
-  
-  const moreBtn = document.createElement('button');
-  moreBtn.style.display = 'block';
-  moreBtn.innerText = 'More';
-  body.appendChild(moreBtn);
-  moreBtn.addEventListener('click', () => {
-    getData();
-    moreBtn.style.display = 'none';
-  });
-}
-
-getData();
+await api.getRandomPhotos();
+ui.appendPhotos(api.randomPhotosAPI);
+ui.createLoadBtn();
--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,8 +1,10 @@
+import { api } from './api';
+
 class UI {
-  constructor(){
-  this.body = document.body;
+  constructor() {
+    this.body = document.body;
   }
-  appendPhotos(data){
+  appendPhotos(data) {
     data.map((photo) => {
       const url = photo.urls.regular;
       const img = document.createElement('img');
@@ -10,7 +12,22 @@ class UI {
       this.body.appendChild(img);
     });
   }
+
+  createLoadBtn() {
+    const Btn = document.createElement('button');
+    Btn.style.display = 'block';
+    Btn.innerText = 'Load More';
+    this.body.appendChild(Btn);
+
+    Btn.addEventListener('click', async () => {
+      // getData();
+      await api.getRandomPhotos();
+      this.appendPhotos(api.randomPhotosAPI);
+      Btn.style.display = 'none';
+      this.appendLoadBtn();
+    });
+  }
 }
 
 const ui = new UI();
-export { ui }
+export { ui };
