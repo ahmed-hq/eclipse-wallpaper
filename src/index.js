6b2ceee58ac9b0be9363b86707d94bf3bee340a4
Ahmed Walid <123060771+ahmed-hq@users.noreply.github.com>
2023-06-26 06:16:36 +0300

    Adding main cta btn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './styles/main.scss';
 import { api } from './modules/api';
 import { ui } from './modules/ui';
 
+ui.createHeroSection()
 await api.getRandomPhotos();
 ui.appendPhotos(api.randomPhotosAPI);
 ui.createLoadBtn();
--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,29 +1,83 @@
-import { api } from './api';
+import { api } from "./api";
+import ctaArrow from "../assets/cta-arrow.svg";
+
+class Element {
+  divCreator(attribute, attName, parent) {
+    const div = document.createElement("div");
+    div.setAttribute(`${attribute}`, `${attName}`);
+    parent.appendChild(div);
+    return div;
+  }
+
+  sectionCreator(attribute, attName, parent) {
+    const section = document.createElement("section");
+    section.setAttribute(`${attribute}`, `${attName}`);
+    parent.appendChild(section);
+    return section;
+  }
+
+  imgCreator(source, parent) {
+    const img = new Image();
+    img.src = source;
+    parent.appendChild(img);
+  }
+}
 
 class UI {
   constructor() {
-    this.body = document.body;
+    this.element = new Element();
+    this.mainWrapper = this.element.divCreator("class", "main-wrapper", document.body);
   }
+  createHeroSection() {
+    const heroSection = this.element.sectionCreator("class", "hero-section", this.mainWrapper);
+    const heroContentWrapper = this.element.divCreator("class", "hero_content-wrapper", heroSection);
+
+    const heroDisplay = document.createElement("h1");
+    heroDisplay.setAttribute("id", "hero-display");
+    heroDisplay.textContent = "Explore";
+    heroContentWrapper.appendChild(heroDisplay);
+
+    const heroSubDisplay = document.createElement("h2");
+    heroSubDisplay.setAttribute("id", "hero-sub_display");
+    heroSubDisplay.textContent = "the Essence of Black in Minimal Wallpapers";
+    heroContentWrapper.appendChild(heroSubDisplay);
+
+    const mainCtaWrapper = this.element.divCreator("class", "main_cta-wrapper", heroSection);
+
+    const mainCta = this.element.divCreator("class", "main_cta", mainCtaWrapper);
+    mainCta.style.border = "1px solid";
+    mainCta.style.borderRadius = "160px";
+    mainCta.style.width = "10rem";
+    mainCta.style.height = "10rem";
+    mainCta.style.display = 'flex'
+    mainCta.style.justifyContent = 'center'
+    mainCta.style.alignItems = 'center'
+
+
+
+    this.element.imgCreator(ctaArrow, mainCta)
+  }
+
   appendPhotos(data) {
     data.map((photo) => {
       const url = photo.urls.regular;
-      const img = document.createElement('img');
-      img.setAttribute('src', `${url}`);
-      this.body.appendChild(img);
+      const img = document.createElement("img");
+      img.setAttribute("src", `${url}`);
+      this.mainWrapper.appendChild(img);
     });
   }
 
   createLoadBtn() {
-    const Btn = document.createElement('button');
-    Btn.style.display = 'block';
-    Btn.innerText = 'Load More';
-    this.body.appendChild(Btn);
+    const Btn = document.createElement("button");
+    Btn.style.display = "block";
+    Btn.innerText = "Load More";
+    this.mainWrapper.appendChild(Btn);
 
-    Btn.addEventListener('click', async () => {
+    Btn.addEventListener("click", async () => {
       // getData();
       await api.getRandomPhotos();
       this.appendPhotos(api.randomPhotosAPI);
-      Btn.style.display = 'none';
+      Btn.style.display = "none";
       this.appendLoadBtn();
     });
   }
